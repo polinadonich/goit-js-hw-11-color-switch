f2789ed8086e620dc0eea6bd7c4ac5fe02a37652
polinadonich <polina.event@mail.ru>
2021-08-30 11:11:48 +0300

    color-switch

---
+++ index.js
@@ -0,0 +1,36 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const bodyBackground = document.querySelector("body");
+const btnStart = document.querySelector('button[data-action="start"]');
+const btnStop = document.querySelector('button[data-action="stop"]');
+
+btnStart.addEventListener("click", startSwitch);
+btnStop.addEventListener("click", stopSwitch);
+
+let timerId = null;
+
+function startSwitch() {
+   timerId = setInterval(() => {
+        bgrSwitch();
+    }, 1000);
+};
+
+function stopSwitch() {
+    clearInterval(timerId);
+};
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function bgrSwitch() {
+    bodyBackground.style.backgroundColor =
+        colors[randomIntegerFromInterval(0, colors.length - 1)];
+}
